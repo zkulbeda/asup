f528c7a62f6dead6f54f554efe53f37e7379c1b4
zkulbeda <zkulbeda@yandex.ru>
2019-05-17 20:01:45 +0300

    123

---
+++ src/renderer/components/PrintCardView.vue
@@ -0,0 +1,85 @@
+<template>
+    <b-card v-show="!loading">
+        <b-row>
+            <b-col class="text-center CreateReportPage">
+                <div class="">
+                    <h2>Создание отчёта</h2>
+                    <b-button class="mt-4" @click="save">Создать</b-button>
+                </div>
+            </b-col>
+        </b-row>
+    </b-card>
+</template>
+
+<script>
+    import {ipcRenderer} from 'electron'
+    import {DateTime} from 'luxon';
+    import jsPDF from 'jspdf';
+    import qr from 'node-qrcode';
+    import promiseIpc from 'electron-promise-ipc';
+
+    export default {
+        name: "CreateReport",
+        data() {
+            return {
+                loading: true,
+                months: [],
+                names: [
+                    'Январь',
+                    'Февраль',
+                    'Март',
+                    'Апрель',
+                    'Май',
+                    'Июнь',
+                    'Июль',
+                    'Август',
+                    'Сентябрь',
+                    'Октябрь',
+                    'Ноябрь',
+                    'Декабрь',
+                ],
+                selected: 0,
+            }
+        },
+        computed: {
+            options() {
+                return this.names.map((e, i) => ({value: i, text: e})).filter((e, i) => this.months[e.value]);
+            }
+        },
+        methods: {
+            save() {
+                var doc = new jsPDF();
+                doc.text('Hello world!', 10, 10);
+                qr.toDataURL('I am a pony!').then((url) => {
+                    doc.addImage(url, 'PNG', 20, 20);
+                    doc.save('a4.pdf');
+                });
+            }
+        },
+        mounted() {
+            console.log(promiseIpc);
+        }
+    }
+</script>
+
+<style scoped>
+    .CreateReportPage {
+        padding: 50px 0px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .CreateReportSelectDiv {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .CreateReportSelect {
+        width: 120px;
+    }
+
+    .CreateReportSelectText {
+        margin-right: 15px;
+    }
+</style>
