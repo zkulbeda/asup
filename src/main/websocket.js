c1efc7cbaad7c01b2ff61f9d06491d69f05e3a0d
zkulbeda <zkulbeda@yandex.ru>
2019-10-29 17:27:29 +0300

    Arduino день два

--- src/main/websocket.js
+++ src/main/websocket.js
@@ -18,59 +18,62 @@ const WebSocket = require('ws');
 //     });
 // }
 
-let evilscan = require('evilscan');
-new evilscan({
-    target: '192.168.100.0/24',
-    port: 57300,
-    status:'O',
-}, function(err, scan){
-    if(err){
-        throw err;
-    }
-    let data = [];
-    scan.on('result', (d)=>{
-        console.log('data', d);
-        data.push(d);
-    })
-    scan.on('done', ()=>{
-        console.log('done');
-        init(data);
-    })
-    scan.run();
+// let evilscan = require('evilscan');
+// new evilscan({
+//     target: '192.168.100.0/24',
+//     port: 57300,
+//     status:'O',
+// }, function(err, scan){
+//     if(err){
+//         throw err;
+//     }
+//     let data = [];
+//     scan.on('result', (d)=>{
+//         console.log('data', d);
+//         data.push(d);
+//     })
+//     scan.on('done', ()=>{
+//         console.log('done');
+//         init(data);
+//     })
+//     scan.run();
+// });
+let port = 57300;
+let wss = new WebSocket.Server({
+    port: 57301
 });
+let connected = []
+wss.on('connection', async (ws, req)=>{
+    ws.send('connect');
+    connected.push(req.connection.remoteAddress);
+    console.log(connected);
+    console.log('connected');
+    ws.on('message', (m)=>{
+        console.log(m);
+    })
+    ws.on('close', (reason)=>{
+        console.log('closed', reason);
+    })
+})
+
+let axios = require('axios');
 let Client = require('node-ssdp').Client, client = new Client();
-client.on('response', function (headers, statusCode, rinfo) {
+client.on('response', async function (headers, statusCode, rinfo) {
     console.log('Got a response to an m-search.');
     console.log(headers, statusCode, rinfo);
-});
-
-client.search('ssdp:all');
-let init = async(data)=>{
-    let wss = new WebSocket.Server({
-        port: 57301
-    });
-
-    wss.on('connection', (ws)=>{
-        ws.send('connect');
-        console.log('connected');
-        ws.on('message', (m)=>{
-            console.log(m);
-        })
-        ws.on('close', (reason)=>{
-            console.log('closed', reason);
-        })
-    })
-    console.log('broadcast server', data);
-    let axios = require('axios');
-    for(let e of data){
-        try {
-            let test = await axios.get('http://' + e.ip + ':' + e.port + '/rfid');
-            if(test.data=='yes'){
-                await axios.get('http://'+e.ip+':'+e.port+'/connect');
-            }
-        }catch (e) {
-            console.log(e);
-        }
+    if(connected.indexOf('::ffff:'+rinfo.address)!=-1){
+        return;
     }
-}
-
+    console.log('connect to '+rinfo.address);
+    try {
+        let test = await axios.get('http://' + rinfo.address + ':' + port + '/rfid');
+        if(test.data=='yes'){
+            await axios.get('http://'+rinfo.address+':'+port+'/connect');
+        }
+    }catch (e) {
+        console.log(e);
+    }
+});
+let interval_id = setInterval(()=>{
+    client.search('urn:rfid-lunch:device:rfid-scanner:1.0');
+}, 5000);
